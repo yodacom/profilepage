c0b251fe8a5d065278f5b7faa5fd711efe3b05a3
Jeremy Black <jb@yodacom.com>
2017-07-05 08:06:13 -0600

    corrected some spelling errors and grammer

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './js/main.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-        ],
-    },
+  entry: './js/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }],
+  },
 };
